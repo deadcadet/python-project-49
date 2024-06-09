b02c5211e8918375b401786de7615819773fbe9c
Artish <artish22@gmail.com>
2024-06-09 17:01:08 +1200

    refactoring

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,5 +1,4 @@
 from brain_games.scripts.game_functions import play_game as play_game
-from brain_games.scripts.game_functions import get_question_calc as get_question_calc  # noqa E501
 
 
 def main():
--- brain_games/games/brain_even.py
+++ brain_games/games/brain_even.py
@@ -1,5 +1,4 @@
 from brain_games.scripts.game_functions import play_game as play_game
-from brain_games.scripts.game_functions import get_question_even as get_question_even  # noqa E501
 
 
 def main():
